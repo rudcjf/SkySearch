c00e46b147578f7303fc3a374be1fe1aa14e1b54
sim4518 <minchae88@gmail.com>
2018-07-12 17:47:42 +0900

    tripInfo 2018/07/12

--- SkySearch/src/main/java/com/skysearch/myapp/service/CityService.java
+++ SkySearch/src/main/java/com/skysearch/myapp/service/CityService.java
@@ -9,22 +9,30 @@ import com.skysearch.myapp.dao.ShareDao;
 @Service
 public class CityService {
 
-	@Autowired
-	private ShareDao dao;
-	
-	public Object getList(Object dataMap) {
-		String sqlMapId = "city.list";
+   @Autowired
+   private ShareDao dao;
+   
+   public Object getCiList(Object dataMap) {
+      String sqlMapId = "city.cilist";
 
-		Object resultObject = dao.getList(sqlMapId, dataMap);
-		
-		return resultObject;
-	}
-	
-	public Object getLocalList(Object dataMap) {
-		String sqlMapId = "city.cityList";
+      Object resultObject = dao.getList(sqlMapId, dataMap);
+      
+      return resultObject;
+   }
+   
+   public Object getCoList(Object dataMap) {
+      String sqlMapId = "city.colist";
 
-		Object resultObject = dao.getList(sqlMapId, dataMap);
-		
-		return resultObject;
-	}
+      Object resultObject = dao.getList(sqlMapId, dataMap);
+      
+      return resultObject;
+   }
+   
+   public Object getLocalList(Object dataMap) {
+      String sqlMapId = "city.cityList";
+
+      Object resultObject = dao.getList(sqlMapId, dataMap);
+      
+      return resultObject;
+   }
 }
